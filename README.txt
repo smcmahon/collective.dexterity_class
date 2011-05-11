bde36f88f14d0eba54ea407a9665630fcab70fde
Steve McMahon <steve@Steve-McMahons-IMac.local>
2011-05-11 13:17:54 -0700

    Baseline zopeskel.dexterity product

---
+++ README.txt
@@ -0,0 +1,7 @@
+Introduction
+============
+
+Examples for Steve McMahon's class on creating Plone content types with
+Dexterity.
+
+This product was produced in a facility known to contain nuts.
---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='collective.dexterity_class',
+      version=version,
+      description="Dexterity Class Examples",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Steve McMahon',
+      author_email='steve@dcn.org',
+      url='https://github.com/smcmahon/collective.dexterity_class',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.dexterity',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins = ["ZopeSkel"],
+
+      )
